2bc1effd7547f141a25a8bc2e8d79437687d225d
asandoval33 <asandoval@develop.com.mx>
2023-05-26 13:59:29 -0500

    subiendo arturoController

---
+++ src/main/java/mx/com/springlabs/arturo/controller/ArturoController.java
@@ -0,0 +1,18 @@
+
+package mx.com.springlabs.arturo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/arturo")
+public class ArturoController {
+    
+    
+     @GetMapping("/pelicula_emoji/{nombrePelicula}")
+    public String prueba(@PathVariable("nombrePelicula") String nombrePelicula) {
+        return nombrePelicula;
+    }
+}
