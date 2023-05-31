1cbbb34d3294589b6b6f5eb79eebb12313e694e8
Usuario <Usuario@Usuario-Gateway>
2023-05-31 12:50:31 -0500

    Teniza

---
+++ src/main/java/mx/com/springlabs/Teniza/controller/TenizaController.java
@@ -0,0 +1,40 @@
+package mx.com.springlabs.Teniza.controller;
+
+//import mx.com.springlabs.controller.*;
+import mx.com.springlabs.Teniza.service.TenizaService;
+//import mx.com.springlabs.service.PruebaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Teniza")
+public class TenizaController {
+
+    
+    @Autowired
+    private TenizaService tenizaService;
+
+    @GetMapping("/grammar")
+    public String prueba() {
+        return tenizaService.grammar();
+    }
+     @GetMapping("/summarize")
+    public String summarize(){
+        return tenizaService.summarize();
+    }
+    @GetMapping("/code")
+    public String code(){
+        return tenizaService.code();
+    }
+      @GetMapping("/sql")
+    public String sql(){
+        return tenizaService.sql();
+    }
+      @GetMapping("/recipe")
+    public String recipe(){
+        return tenizaService.recipe();
+    }
+}
