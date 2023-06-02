dd2c2a060e302760fc40db3e8ee9e810913043c0
juanj <juanj@172.16.0.249>
2023-06-02 12:56:14 -0500

    Ahí va mi parte

---
+++ src/main/java/mx/com/springlabs/juanjose/controller/controllerJuanJose.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.com.springlabs.juanjose.controller;
+
+import mx.com.springlabs.juanjose.service.ServiceJuan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author juanj
+ */
+
+@RestController
+@RequestMapping("/pruebasJuan")
+
+public class controllerJuanJose {
+
+
+    @Autowired
+    private ServiceJuan ServiceJuan;
+
+    @GetMapping("/NaturalLanguage/")
+    public String prueba() {
+        return ServiceJuan.naturalLanguage();
+    }
+    
+    @GetMapping("/productNameGenerator/")
+    public String productName() {
+        return ServiceJuan.productNameGenerator();
+    }
+    
+    @GetMapping("/moodToColor/")
+    public String moodColor() {
+        return ServiceJuan.moodColor();
+    }
+    
+    @GetMapping("/restaurantReviewCreator/")
+    public String restaurantReviewCreator() {
+        return ServiceJuan.restaurantReviewCreator();
+    }
+
+}
+
---
+++ src/main/java/mx/com/springlabs/juanjose/service/ServiceImplJuanJose.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package mx.com.springlabs.juanjose.service;
+import mx.com.springlabs.models.CompletionsRequestModel;
+import mx.com.springlabs.repository.OpenAIRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+/**
+ *
+ * @author juanj
+ */
+@Service
+public class ServiceImplJuanJose implements ServiceJuan {
+    @Autowired
+    OpenAIRepository openAIRepository;
+
+    @Override
+    public String naturalLanguage() {
+        CompletionsRequestModel crm = new CompletionsRequestModel();
+        crm.setModel("text-davinci-003");
+        crm.setPrompt("\"\"\"\nUtil exposes the following:\n\nutil.stripe() -> authenticates & returns the stripe module; usable as stripe.Charge.create etc\n\"\"\"\nimport util\n\"\"\"\nCreate a Stripe token using the users credit card: 5555-4444-3333-2222, expiration date 12 / 28, cvc 521\n\"\"\"");
+        crm.setTemperature(0.0);
+        crm.setMax_tokens(100);
+        crm.setTop_p(1.0);
+        crm.setFrequency_penalty(0.0);
+        crm.setPresence_penalty(0.0);
+        String[] stop = {"\"\"\""};
+        crm.setStop(stop);
+        return openAIRepository.ejecutaSolicitud(crm).getChoices()[0].getText();
+    }
+    
+    @Override
+    public String productNameGenerator() {
+        CompletionsRequestModel crm = new CompletionsRequestModel();
+        crm.setModel("text-davinci-003");
+        crm.setPrompt("Product description: A home milkshake maker\\nSeed words: fast, healthy, compact.\\nProduct names: HomeShaker, Fit Shaker, QuickShake, Shake Maker\\n\\nProduct description: A pair of shoes that can fit any foot size.\\nSeed words: adaptable, fit, omni-fit.");
+        crm.setTemperature(0.8);
+        crm.setMax_tokens(60);
+        crm.setTop_p(1.0);
+        crm.setFrequency_penalty(0.0);
+        crm.setPresence_penalty(0.0);
+        return openAIRepository.ejecutaSolicitud(crm).getChoices()[0].getText();
+    }
+    
+    @Override
+    public String moodColor() {
+        CompletionsRequestModel crm = new CompletionsRequestModel();
+        crm.setModel("text-davinci-003");
+        crm.setPrompt("The CSS code for a color like a blue sky at dusk:\\n\\nbackground-color: #");
+        crm.setTemperature(0.0);
+        crm.setMax_tokens(64);
+        crm.setTop_p(1.0);
+        crm.setFrequency_penalty(0.0);
+        crm.setPresence_penalty(0.0);
+        return openAIRepository.ejecutaSolicitud(crm).getChoices()[0].getText();
+    }
+    
+    @Override
+    public String restaurantReviewCreator() {
+        CompletionsRequestModel crm = new CompletionsRequestModel();
+        crm.setModel("text-davinci-003");
+        crm.setPrompt("Write a restaurant review based on these notes:\\n\\nName: The Blue Wharf\\nLobster great, noisy, service polite, prices good.\\n\\nReview:");
+        crm.setTemperature(0.5);
+        crm.setMax_tokens(64);
+        crm.setTop_p(1.0);
+        crm.setFrequency_penalty(0.0);
+        crm.setPresence_penalty(0.0);
+        return openAIRepository.ejecutaSolicitud(crm).getChoices()[0].getText();
+    }
+}
