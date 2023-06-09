5ed50a9f8f1df05df141db5e3ff28597ad9355b4
arnol <arnol@LAPTOP-A345AQ9A>
2023-06-09 12:30:10 -0600

    Arnol

--- src/main/java/mx/com/springlabs/arnol/controller/ArnolController.java
+++ src/main/java/mx/com/springlabs/arnol/controller/ArnolController.java
@@ -3,12 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package mx.com.springlabs.arnol.controller;
+import java.io.IOException;
 import mx.com.springlabs.arnol.service.arnolServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 /**
  *
  * @author arnol
@@ -19,18 +25,27 @@ public class ArnolController {
     @Autowired
     private arnolServiceInterface arnolserviceinterface;
     
-    @GetMapping("/Natural_language_to_OpenAI_API/{datos}")
-    /*
-    Natural_language_to_OpenAI_API
-    Keywords
-    Extract_contact_information
-    Chat
-
-    */
-    public String prueba(@PathVariable("datos") String nombrePelicula) {
-        return arnolserviceinterface.natural_language_to_OpenAI_API(nombrePelicula);
+    @GetMapping("/natural_language_to_OpenAI_API/{datos}")
+    public String natural_language_to_OpenAI_API(@PathVariable("datos") String datos) {
+        return arnolserviceinterface.natural_language_to_OpenAI_API(datos);
         
     }
     
-
+    @GetMapping("/keywords/{datos}")
+    public String keywords(@PathVariable("datos") String datos) {
+        return arnolserviceinterface.keywords(datos);
+        
+    }
+    @GetMapping("/extractContactInformation/{datos}")
+    public String extractContactInformation(@PathVariable("datos") String datos) {
+        return arnolserviceinterface.extractContactInformation(datos);
+        
+    }
+    @GetMapping("/chat/{datos}")
+    public String chat(@PathVariable("datos") String datos) {
+        return arnolserviceinterface.chat(datos);
+        
+    }
+    
+    
 }
